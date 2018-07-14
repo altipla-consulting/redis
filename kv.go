6c9ec8d020f347b434af8b48c59fa6dc162ba561
Ernesto Alejo <contacto@ernestoalejo.me>
2018-07-14 14:06:59 +0200

    Add methods to set a key-value with a TTL. (#5)

--- kv.go
+++ kv.go
@@ -2,6 +2,7 @@ package redis
 
 import (
 	"strconv"
+	"time"
 
 	"github.com/golang/protobuf/proto"
 	"github.com/juju/errors"
@@ -14,6 +15,10 @@ type StringKV struct {
 
 func (kv *StringKV) Set(value string) error {
 	return errors.Trace(kv.db.sess.Set(kv.key, value, 0).Err())
+}
+
+func (kv *StringKV) SetTTL(value string, ttl time.Duration) error {
+	return errors.Trace(kv.db.sess.Set(kv.key, value, ttl).Err())
 }
 
 func (kv *StringKV) Get() (string, error) {
@@ -41,6 +46,10 @@ type Int32KV struct {
 
 func (kv *Int32KV) Set(value int32) error {
 	return errors.Trace(kv.db.sess.Set(kv.key, value, 0).Err())
+}
+
+func (kv *Int32KV) SetTTL(value int32, ttl time.Duration) error {
+	return errors.Trace(kv.db.sess.Set(kv.key, value, ttl).Err())
 }
 
 func (kv *Int32KV) Get() (int32, error) {
@@ -75,6 +84,15 @@ func (kv *ProtoKV) Set(value proto.Message) error {
 	return errors.Trace(kv.db.sess.Set(kv.key, string(bytes), 0).Err())
 }
 
+func (kv *ProtoKV) SetTTL(value proto.Message, ttl time.Duration) error {
+	bytes, err := proto.Marshal(value)
+	if err != nil {
+		return errors.Trace(err)
+	}
+
+	return errors.Trace(kv.db.sess.Set(kv.key, string(bytes), ttl).Err())
+}
+
 func (kv *ProtoKV) Get(value proto.Message) error {
 	result, err := kv.db.sess.Get(kv.key).Result()
 	if err != nil {
@@ -100,6 +118,10 @@ type BooleanKV struct {
 
 func (kv *BooleanKV) Set(value bool) error {
 	return errors.Trace(kv.db.sess.Set(kv.key, value, 0).Err())
+}
+
+func (kv *BooleanKV) SetTTL(value bool, ttl time.Duration) error {
+	return errors.Trace(kv.db.sess.Set(kv.key, value, ttl).Err())
 }
 
 func (kv *BooleanKV) Get() (bool, error) {

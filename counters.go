ff9d37ae02cfa7099be71e7b1daaf353dc3c4073
Ernesto Alejo <ernestokarim@gmail.com>
2018-07-21 15:24:31 +0200

    Remove dependency on external errors library.

--- counters.go
+++ counters.go
@@ -2,8 +2,6 @@ package redis
 
 import (
 	"fmt"
-
-	"github.com/juju/errors"
 )
 
 type Counters struct {
@@ -24,26 +22,26 @@ type Counter struct {
 }
 
 func (c *Counter) Set(value int64) error {
-	return errors.Trace(c.db.sess.Set(c.key, value, 0).Err())
+	return c.db.sess.Set(c.key, value, 0).Err()
 }
 
 func (c *Counter) Get() (int64, error) {
 	result, err := c.db.sess.Get(c.key).Int64()
 	if err != nil {
-		return 0, errors.Trace(err)
+		return 0, err
 	}
 
 	return result, nil
 }
 
 func (c *Counter) Increment() error {
-	return errors.Trace(c.IncrementBy(1))
+	return c.IncrementBy(1)
 }
 
 func (c *Counter) Decrement() error {
-	return errors.Trace(c.IncrementBy(-1))
+	return c.IncrementBy(-1)
 }
 
 func (c *Counter) IncrementBy(value int64) error {
-	return errors.Trace(c.db.sess.IncrBy(c.key, value).Err())
+	return c.db.sess.IncrBy(c.key, value).Err()
 }
--- proto_hash.go
+++ proto_hash.go
@@ -1,11 +1,11 @@
 package redis
 
 import (
+	"fmt"
 	"reflect"
 
 	"github.com/go-redis/redis"
 	"github.com/golang/protobuf/proto"
-	"github.com/juju/errors"
 )
 
 type ProtoHash struct {
@@ -27,7 +27,7 @@ func (hash *ProtoHash) GetMulti(keys []string, result interface{}) error {
 	rv := reflect.ValueOf(result)
 	msg := reflect.TypeOf((*proto.Message)(nil)).Elem()
 	if rt.Kind() != reflect.Ptr || rt.Elem().Kind() != reflect.Slice || !rt.Elem().Elem().Implements(msg) {
-		return errors.Errorf("redis: expected a pointer to a slice for the result, received %T", result)
+		return fmt.Errorf("redis: expected a pointer to a slice for the result, received %T", result)
 	}
 
 	dest := reflect.MakeSlice(rt.Elem(), 0, 0)
@@ -36,7 +36,7 @@ func (hash *ProtoHash) GetMulti(keys []string, result interface{}) error {
 
 	redisResult, err := hash.db.sess.HMGet(hash.key, keys...).Result()
 	if err != nil {
-		return errors.Trace(err)
+		return err
 	}
 	for _, item := range redisResult {
 		var model reflect.Value
@@ -46,7 +46,7 @@ func (hash *ProtoHash) GetMulti(keys []string, result interface{}) error {
 		} else {
 			model = reflect.New(rt.Elem().Elem().Elem())
 			if err := proto.Unmarshal([]byte(item.(string)), model.Interface().(proto.Message)); err != nil {
-				return errors.Trace(err)
+				return err
 			}
 			merr = append(merr, nil)
 		}
@@ -69,14 +69,10 @@ func (hash *ProtoHash) Get(key string, model proto.Message) error {
 			return ErrNoSuchEntity
 		}
 
-		return errors.Trace(err)
+		return err
 	}
 
-	if err := proto.Unmarshal([]byte(redisResult), model); err != nil {
-		return errors.Trace(err)
-	}
-
-	return nil
+	return proto.Unmarshal([]byte(redisResult), model)
 }
 
 type ProtoHashInsert struct {
@@ -87,7 +83,7 @@ type ProtoHashInsert struct {
 func (insert *ProtoHashInsert) Set(key string, value proto.Message) error {
 	bytes, err := proto.Marshal(value)
 	if err != nil {
-		return errors.Trace(err)
+		return err
 	}
 
 	insert.fields[key] = string(bytes)
@@ -96,5 +92,5 @@ func (insert *ProtoHashInsert) Set(key string, value proto.Message) error {
 }
 
 func (insert *ProtoHashInsert) Commit() error {
-	return errors.Trace(insert.hash.db.sess.HMSet(insert.hash.key, insert.fields).Err())
+	return insert.hash.db.sess.HMSet(insert.hash.key, insert.fields).Err()
 }

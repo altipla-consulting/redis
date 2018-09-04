36aeb643ecb0fd821cf86da1732d2d3c51947e42
Alberto Moreno <albertinivva@gmail.com>
2018-09-04 07:05:04 +0200

    Add Int64 set.

--- sets.go
+++ sets.go
@@ -98,31 +98,3 @@ func (set *Int64Set) Remove(values ...int64) error {
 
 	return set.db.sess.SRem(set.key, members...).Err()
 }
-
-func (set *Int64Set) SortAlpha() ([]int64, error) {
-	result, err := set.sort(&redis.Sort{Alpha: true})
-	if err != nil {
-		return nil, err
-	}
-
-	return result, nil
-}
-
-func (set *Int64Set) sort(sort *redis.Sort) ([]int64, error) {
-	rawResult, err := set.db.sess.Sort(set.key, sort).Result()
-	if err != nil {
-		return nil, err
-	}
-
-	result := []int64{}
-	for _, r := range rawResult {
-		n, err := strconv.ParseInt(r, 10, 64)
-		if err != nil {
-			return nil, err
-		}
-
-		result = append(result, n)
-	}
-
-	return result, nil
-}
